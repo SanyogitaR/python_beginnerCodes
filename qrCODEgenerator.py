81df369438ce6e4aab4c27d74da14642fdb25a8a
Sanyogita R <161912856+SanyogitaR@users.noreply.github.com>
2024-06-09 09:30:01 +0530

    Add files via upload

---
+++ qrCODEgenerator.py
@@ -0,0 +1,3 @@
+import qrcode as qr
+img=qr.make("https://www.youtube.com/@Drewinkle3703")
+img.save("DREWINKLE YT channel.png")
---
+++ qrCOSEgenerator2.py
@@ -0,0 +1,13 @@
+import qrcode
+from PIL import Image
+import qrcode.constants
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=20,
+    border=4,
+)
+qr.add_data("https://www.youtube.com/@Drewinkle3703")
+qr.make(fit=True)
+img=qr.make_image(fill_color="purple", back_color="white")
+img.save("DREWINKLE YT channel_2 .png")
